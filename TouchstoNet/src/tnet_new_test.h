58b454cffb0abad1f09213f95dfd5804adf9ec71
Janusz <januszvdm@gmail.com>
2021-10-15 23:14:59 +0200

    Support for IP address argument started

--- TouchstoNet/src/tnet_new_test.h
+++ TouchstoNet/src/tnet_new_test.h
@@ -30,6 +30,7 @@ struct tnet_new_test {
 	struct sockaddr_in* server_socket_address_stuct;
 	struct sockaddr_in* client_socket_address_stuct;
 	int32_t port_no;
+	in_addr_t ip_address;
 };
 
 bool tnet_create_new_test(struct tnet_new_test** new_test);
