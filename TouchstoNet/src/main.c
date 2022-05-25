f17180c6cf09e2d85c82e245613f3001746f55a7
jwolak <januszvdm@gmail.com>
2022-05-25 10:08:38 +0200

    TouchstoNetEngine created

--- TouchstoNet/src/main.c
+++ TouchstoNet/src/main.c
@@ -17,13 +17,27 @@
 #include <setjmp.h>
 
 #include "LoggerC.h"
+#include "TouchstoNet-Engine.h"
 
 static jmp_buf sigend_jmp_buf;
 
 static void __attribute__ ((noreturn)) sigend_handler(int sig)
 {
-	/*tnet_debug("%s %d %s", "Signal", sig, "caught");*/
+    LOG_WARNING("%s%d%s", "Signal", sig, "caught");
     longjmp(sigend_jmp_buf, 1);
+}
+
+static void tnet_catch_sigend(void (*handler)(int))
+{
+#ifdef SIGINT
+    signal(SIGINT, handler);
+#endif
+#ifdef SIGTERM
+    signal(SIGTERM, handler);
+#endif
+#ifdef SIGHUP
+    signal(SIGHUP, handler);
+#endif
 }
 
 int main(int argc, char **argv) {
@@ -31,10 +45,26 @@ int main(int argc, char **argv) {
   SET_LOG_LEVEL(DEBUG);
   SET_LOG_LOGGER_OUTPUT(CONSOLE_AND_FILE);
 
-/*	tnet_catch_sigend(sigend_handler);
+  struct TouchstoNetEngine touchstonet_engine =  TouchstoNetEngine.new();
+
+	tnet_catch_sigend(sigend_handler);
 	if (setjmp(sigend_jmp_buf)){
-		tnet_signal_handler(new_test);
-	}*/
+
+		if (!touchstonet_engine.stop(&touchstonet_engine)) {
+		  LOG_WARNING("%s", "Failed to stop TouchstoNet engine");
+		}
+
+		LOG_WARNING("%s", "TouchstoNet engine interrupted by user");
+	}
+
+  if (!touchstonet_engine.start(&touchstonet_engine, argc, argv)) {
+
+    LOG_ERROR("%s", "Failed to start TouchstoNet engine");
+    exit(1);
+  } else {
+    LOG_DEBUG("%s", "TouchstoNet started successfully");
+  }
+
 
     return 0;
 }
