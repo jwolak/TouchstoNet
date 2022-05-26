9bfc8842279c3ba9cda79aec847d5c00a871d838
jwolak <januszvdm@gmail.com>
2022-05-26 17:14:04 +0200

    Settings tests implementation started

--- UnitTests/Main-Tests-Run.c
+++ UnitTests/Main-Tests-Run.c
@@ -21,13 +21,31 @@ int main(void) {
   SET_LOG_LOGGER_OUTPUT(CONSOLE_AND_FILE);
 
     UNITY_BEGIN();
-/*    RUN_TEST(get_role_test);
+/*
     RUN_TEST(set_callback_and_callback_function_is_called_after_2s_timeout);
     RUN_TEST(set_timeout_2s_and_set_time_is_elapsed);
     RUN_TEST(set_longer_timeout_14s_and_set_time_is_elapsed);
     RUN_TEST(start_counter_for_14_seconds_and_stop_it_after_5_seconds);
     RUN_TEST(start_touchstone_engine_and_true_returned);
     RUN_TEST(start_and_stop_touchstone_engine_and_true_returned);*/
+    RUN_TEST(call_settings_get_role_and_role_is_returned);
+    RUN_TEST(call_settings_get_port_number_and_port_number_is_returned);
+    RUN_TEST(call_settings_get_ip_address_and_ip_address_is_returned);
+    RUN_TEST(call_settings_get_test_duration_and_test_duration_is_returned);
+    RUN_TEST(call_set_role_with_server_mode_and_server_mode_is_set);
+    RUN_TEST(call_set_role_with_server_mode_and_true_is_returned);
+    RUN_TEST(call_set_role_with_client_mode_and_server_mode_is_set);
+    RUN_TEST(call_set_role_with_client_mode_and_true_is_returned);
+    RUN_TEST(try_call_set_role_with_invalid_mode_and_false_is_returned);
+    RUN_TEST(call_set_port_number_and_port_number_is_set);
+    RUN_TEST(call_set_port_number_and_true_is_returned);
+    RUN_TEST(try_set_port_number_with_invalid_to_big_value_and_false_is_returned);
+    RUN_TEST(try_set_port_number_with_invalid_negative_value_and_false_is_returned);
+    RUN_TEST(call_set_port_number_with_max_value_and_port_number_is_set);
+    RUN_TEST(call_set_port_number_with_min_value_and_port_number_is_set);
+    RUN_TEST(call_set_ip_address_with_valid_address_and_true_is_returned);
+    RUN_TEST(call_set_ip_address_with_invalid_address_and_false_is_returned);
+
     RUN_TEST(inject_settings_to_instance_with_value_and_the_value_in_instance_is_the_same);
     RUN_TEST(inject_settings_to_instance_and_it_is_successful_and_true_returned);
     RUN_TEST(try_inject_null_pointer_of_settings_to_instance_and_false_returned);
